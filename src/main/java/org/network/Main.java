6694255df28b4c618a1044384dc1898646215798
kimshinseung <94797657+kimshinseung@users.noreply.github.com>
2022-11-05 19:35:41 +0900

    시작화면 gif로

--- src/main/java/org/network/Main.java
+++ src/main/java/org/network/Main.java
@@ -1,9 +1,13 @@
 package org.network;
 
+import org.network.packet.startFrame;
+
+
 public class Main {
     public static void main(String[] args)
     {
         LoginFrame loginFrame = new LoginFrame();
         loginFrame.setUserSocket(new UserSocket());
+
     }
 }

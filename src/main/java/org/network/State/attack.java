66857258c5bd255b3640f3133bbbb4f54f5eb31f
kimshinseung <94797657+kimshinseung@users.noreply.github.com>
2022-12-01 11:06:45 +0900

    state

---
+++ src/main/java/org/network/State/attack.java
@@ -0,0 +1,8 @@
+package org.network.State;
+
+public class attack implements State{
+    @Override
+    public void click() {
+
+    }
+}
---
+++ src/main/java/org/network/State/changePocketmon.java
@@ -0,0 +1,8 @@
+package org.network.State;
+
+public class changePocketmon implements State{
+    @Override
+    public void click() {
+
+    }
+}
